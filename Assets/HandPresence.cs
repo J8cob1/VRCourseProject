60cef69c354330de4db5b6d46f4359ccca8fb113
MatthewCole-PDX <macole@pdx.edu>
2021-03-10 22:18:43 -0800

    rebuilt player

---
+++ Assets/HandPresence.cs
@@ -0,0 +1,90 @@
+﻿//source "Introduction to VR in Unity Part 2: Input and Hand Presence", https://www.youtube.com/watch?v=VdT0zMcggTQ&t=677s
+
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.XR;
+
+
+public class HandPresence: MonoBehaviour
+{
+    public bool showController = false;
+    public InputDeviceCharacteristics controllerCharacteristics;
+    public List<GameObject> controllerPrefabs;
+    public GameObject handModelPrefab;
+
+    private InputDevice targetDevice;
+    private GameObject spawnedController;
+    private GameObject spawnedHandModel;
+    private Animator handAnimator;
+    // Start is called before the first frame update
+    void Start()
+    {
+        List<InputDevice> devices = new List<InputDevice>();
+        // InputDeviceCharacteristics leftControllerCharacteristics = InputDeviceCharacteristics.Left | InputDeviceCharacteristics.Controller;
+        InputDevices.GetDevicesWithCharacteristics(controllerCharacteristics, devices);
+        foreach (var item in devices)
+        {
+            Debug.Log(item.name + item.characteristics);
+        }
+        targetDevice = devices[0];
+        GameObject prefab = controllerPrefabs.Find(controller => controller.name == targetDevice.name);
+        //InputDevices.GetDevices(devices);
+        if (prefab)
+        {
+            spawnedController = Instantiate(prefab, transform);
+        }
+        else
+            spawnedController = Instantiate(controllerPrefabs[0], transform);
+        
+        spawnedHandModel = Instantiate(handModelPrefab, transform);
+        handAnimator = spawnedHandModel.GetComponent<Animator>();
+    }
+
+    void UpdateHandAnimation()
+    {
+        if(targetDevice.TryGetFeatureValue(CommonUsages.trigger, out float triggerValue))
+        {
+            handAnimator.SetFloat("Trigger", triggerValue);
+        }
+        else
+        {
+            handAnimator.SetFloat("Trigger", 0);
+        }
+        if (targetDevice.TryGetFeatureValue(CommonUsages.grip, out float gripValue))
+        {
+            handAnimator.SetFloat("Grip", gripValue);
+        }
+        else
+        {
+            handAnimator.SetFloat("Grip", 0);
+        }
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        if (showController)
+        {
+            spawnedHandModel.SetActive(false);
+            spawnedController.SetActive(true);
+        }
+        else
+        {
+            spawnedHandModel.SetActive(true);
+            spawnedController.SetActive(false);
+            UpdateHandAnimation();
+        }
+        if (targetDevice.TryGetFeatureValue(CommonUsages.grip, out float gripValue) && gripValue > 0.1f)
+            Debug.Log("Squeezing Grip " + gripValue);
+        if (targetDevice.TryGetFeatureValue(CommonUsages.gripButton, out bool gripButtonValue) && gripButtonValue)
+            Debug.Log("Pressing Grip Button");
+        /*
+        if (targetDevice.TryGetFeatureValue(CommonUsages.primaryButton, out bool primaryButtonValue) && primaryButtonValue)
+            Debug.Log("Pressing Primary Button");
+        if (targetDevice.TryGetFeatureValue(CommonUsages.trigger, out float triggerValue) && triggerValue > 0.1f)
+            Debug.Log("Trigger pressed " + triggerValue);
+        if (targetDevice.TryGetFeatureValue(CommonUsages.primary2DAxis, out Vector2 primary2DAxisValue) && primary2DAxisValue != Vector2.zero)
+            Debug.Log("Primary Touchpad " + primary2DAxisValue);*/
+    }
+}
---
+++ Assets/LeftHandPresence.cs
@@ -0,0 +1,44 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.XR;
+
+
+public class LeftHandPresence : MonoBehaviour
+{
+    public List<GameObject> controllerPrefabs;
+    private InputDevice targetDevice;
+    private GameObject spawnController;
+    // Start is called before the first frame update
+    void Start()
+    {
+        List<InputDevice> devices = new List<InputDevice>();
+        InputDeviceCharacteristics leftControllerCharacteristics = InputDeviceCharacteristics.Left | InputDeviceCharacteristics.Controller;
+        InputDevices.GetDevicesWithCharacteristics(leftControllerCharacteristics, devices);
+        foreach (var item in devices)
+        {
+            Debug.Log(item.name + item.characteristics);
+        }
+        targetDevice = devices[0];
+        GameObject prefab = controllerPrefabs.Find(controller => controller.name == targetDevice.name);
+        //InputDevices.GetDevices(devices);
+        if (prefab)
+        {
+            spawnController = Instantiate(prefab, transform);
+        }
+        else
+            spawnController = Instantiate(controllerPrefabs[0], transform);
+
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        if (targetDevice.TryGetFeatureValue(CommonUsages.primaryButton, out bool primaryButtonValue) && primaryButtonValue)
+            Debug.Log("Pressing Primary Button");
+        if (targetDevice.TryGetFeatureValue(CommonUsages.trigger, out float triggerValue) && triggerValue > 0.1f)
+            Debug.Log("Trigger pressed " + triggerValue);
+        if (targetDevice.TryGetFeatureValue(CommonUsages.primary2DAxis, out Vector2 primary2DAxisValue) && primary2DAxisValue != Vector2.zero)
+            Debug.Log("Primary Touchpad " + primary2DAxisValue);
+    }
+}
